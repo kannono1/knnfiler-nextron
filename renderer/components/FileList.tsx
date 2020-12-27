1650ba9e8e7741a001594ecc1d5a08529490dc11
Kanno Kazuyuki <kannoweb@gmail.com>
2020-12-28 08:56:51 +0900

    file list row

--- renderer/components/FileList.tsx
+++ renderer/components/FileList.tsx
@@ -10,12 +10,13 @@ const style: React.CSSProperties = {
     width: '50%',
 };
 
+const arr = new Array(20).fill(null).map((_, i) => i);
+
 const FileList: React.FC<Props> = ({ wid }) => {
     return (
         <div style={style}>
             <CurrentDir wid={wid}></CurrentDir>
-            <FileListRow wid={wid} no={0}></FileListRow>
-            <FileListRow wid={wid} no={1}></FileListRow>
+            {arr.map((no) => <FileListRow wid={wid} no={no}></FileListRow>)}
         </div>
     );
 };
--- renderer/components/FileListRow.tsx
+++ renderer/components/FileListRow.tsx
@@ -5,31 +5,49 @@ type Props = {
     no: number;
 }
 
-const styles: {[key:string]:React.CSSProperties} = {
-    wrapper:{
+const styles: { [key: string]: React.CSSProperties } = {
+    wrapper: {
         display: 'flex',
-        border: '1px solid',
     },
-    filename:{
+    filename: {
         width: '50%',
-        backgroundColor: 'red',
     },
-    filesize:{
+    filesize: {
         width: '20%',
-        backgroundColor: 'yellow',
     },
-    updated:{
+    updated: {
         width: '20%',
-        backgroundColor: 'blue',
     },
 };
 
+const stylesActive: { [key: string]: React.CSSProperties } = {
+    wrapper: {
+        display: 'flex',
+    },
+    filename: {
+        width: '50%',
+        backgroundColor: 'green',
+    },
+    filesize: {
+        width: '20%',
+        backgroundColor: 'green',
+    },
+    updated: {
+        width: '20%',
+        backgroundColor: 'green',
+    },
+};
+
+const isActive = (wid, no) => {
+    return (wid == 0 && no == 0);
+}
+
 const FileListRow: React.FC<Props> = ({ wid, no }) => {
     return (
-        <div style={styles.wrapper}>
-            <div style={styles.filename}>FileName{wid}-{no}</div>
-            <div style={styles.filesize}>FileSize{wid}</div>
-            <div style={styles.updated}>Updated{wid}</div>
+        <div style={(isActive(wid, no)) ? stylesActive.wrapper : styles.wrapper}>
+            <div style={(isActive(wid, no)) ? stylesActive.filename : styles.filename}>FileName{wid}-{no}</div>
+            <div style={(isActive(wid, no)) ? stylesActive.filesize : styles.filesize}>FileSize{wid}</div>
+            <div style={(isActive(wid, no)) ? stylesActive.updated : styles.updated}>Updated{wid}</div>
         </div>
     );
 };
