8a9e63505c763aac9254bff4f37f75c8a1e608ba
Kanno Kazuyuki <kannoweb@gmail.com>
2020-12-29 04:14:11 +0900

    key event

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import Head from 'next/head';
 import FileLists from '../components/FileLists';
 
 const Home = () => {
+  const handleUserKeyPress = useCallback(event => {
+    const { key, keyCode } = event;
+    console.log('keydown', key, keyCode);
+    switch (key) {
+      case 'j': console.log('=== j'); break;
+      default: break;
+    }
+  }, []);
+
+  const handleUserKeyUp = useCallback(event => {
+    const { key, keyCode } = event;
+    console.log('keyup', key, keyCode);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleUserKeyPress, false);
+    document.addEventListener('keyup', handleUserKeyUp, false);
+  });
+
   return (
     <React.Fragment>
       <Head>
